f9889e91b9981c53ba24678a1ddb0d9cd44e974c
micah hodge <micah hodge>
2017-01-15 11:57:27 -0600

    adding initial simple projectile and updated gitignore file.

---
+++ Assets/Scripts/Projectiles/fireProjectile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireProjectile : MonoBehaviour {
+
+    public GameObject prefab;
+    public GameObject spawnpoint;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Fire();
+        }
+
+    }
+
+    public void Fire()
+    {
+        GameObject.Instantiate(prefab, spawnpoint.transform.position + .01f * transform.forward, transform.rotation);
+        
+    }
+}
