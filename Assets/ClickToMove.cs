9bfb4532f6d72b161b711561e86b097ff25d5d75
Tim <Spin16@gmail.com>
2017-01-26 00:33:09 -0600

    Revert "Let's start from scratch"
    This reverts commit 1c260ccdd887652f1e7383be8b7d6ef0f6063b9c.

---
+++ Assets/ClickToMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickToMove : MonoBehaviour {
+
+	private Animator anim;
+	private NavMeshAgent navMeshAgent;
+	private Transform targetedEnemy;
+	private Ray shootRay;
+	private RaycastHit shootHit;
+	private bool walking;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		anim = GetComponent<Animator> ();
+		navMeshAgent = GetComponent<NavMeshAgent> (); 
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+		if (Input.GetButtonDown ("Fire1")) 
+		{
+			if (Physics.Raycast (ray, out hit, 100)) 
+			{				
+				walking = true;
+				navMeshAgent.destination = hit.point;
+				navMeshAgent.Resume ();
+			}
+		}
+
+		anim.SetBool ("IsWalking", walking);
+
+	}
+}
---
+++ Assets/Scripts/Projectiles/simpleProjectileBehavior.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class simpleProjectileBehavior : MonoBehaviour {
+
+    public int speed = 25;
+    public int dmgMultiplier = 15;
+    public int explosionForce = 100;
+    public int explosionRadius = 2;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0, 0, speed * Time.deltaTime, Space.Self);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        List<string> affectedPlayers = new List<string>();
+
+        //if (other.tag != "Enemy")
+         //   return;
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+
+        // Go through all the colliders...       
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            Transform rootTransform = colliders[i].transform.root;
+            if (affectedPlayers.Contains(rootTransform.name))
+                continue;
+
+            Rigidbody targetRigidbody = rootTransform.GetComponent<Rigidbody>();
+            //player_Health playerHealth = rootTransform.GetComponent<player_Health>();
+            // If they don't have a rigidbody, go on to the next collider.
+            if (!targetRigidbody)
+                continue;
+                   
+            // Add an explosion force.
+            affectedPlayers.Add(rootTransform.name);            
+            targetRigidbody.AddExplosionForce(explosionForce, transform.position, explosionRadius);
+            
+            Destroy(gameObject);
+        }
+    }
+    }
